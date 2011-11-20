3dfad3b7724cdef3d9b2fc2a07011f4328e098b9
Mark Ransom <mark@apprabbit.com>
2011-11-19 22:23:01 -0600

    We now only allow a single theme to be active for a site at a given time.

--- cms_themes/__init__.py
+++ cms_themes/__init__.py
@@ -3,9 +3,14 @@ __version__ = "1.0.5"
 import random 
 import os
 
-def init_themes():
+try:
+    from django.conf import settings
     from django.contrib.sites.models import Site
     from cms.conf.patch import post_patch
+except:
+    pass
+    
+def init_themes():
 
     if not hasattr(settings, 'THEMES_DIR'):
         THEMES_DIR = os.path.join(settings.PROJECT_DIR, 'themes')
@@ -19,29 +24,33 @@ def init_themes():
         setattr(settings, 'DEFAULT_CMS_TEMPLATES', settings.CMS_TEMPLATES)
     if settings.THEMES_DIR not in settings.TEMPLATE_DIRS:
         settings.TEMPLATE_DIRS = settings.TEMPLATE_DIRS + (settings.THEMES_DIR,)
-
-def set_themes():
-    try:
-        site = Site.objects.get(id=settings.SITE_ID)
-        themes = [theme[0] for theme in site.theme_set.values_list('name')]
-    except: 
-        themes = []
-    theme_templates = []
+    if not hasattr(settings, 'DEFAULT_TEMPLATE_DIRS'):
+        setattr(settings, 'DEFAULT_TEMPLATE_DIRS', settings.TEMPLATE_DIRS)
     
+def set_themes():
+    if not Site.objects.filter(id=settings.SITE_ID):
+        return
+
+    site = Site.objects.get(id=settings.SITE_ID)
+    themes = [theme.name for theme in site.theme_set.all()]
+
+    if not themes: 
+        return
+
+    theme_templates = []
     for theme_dir in os.listdir(settings.THEMES_DIR):
-        if theme_dir in themes or not themes:
+        if theme_dir in themes:
             theme_full_path = os.path.join(settings.THEMES_DIR, theme_dir)
             if os.path.isdir(theme_full_path) and 'templates' in os.listdir(theme_full_path):
                 template_path = os.path.join(theme_full_path, 'templates')
+                setattr(settings, 'TEMPLATE_DIRS', (template_path,) + settings.DEFAULT_TEMPLATE_DIRS)
                 for template in os.listdir(template_path):
-                    template_path = os.path.join(theme_dir, 'templates', template)
                     template_display = '%s (%s)' % (template.replace('_', ' ').title().split('.')[0], theme_dir)
-                    theme_templates.append((template_path, template_display))
+                    theme_templates.append((template, template_display))
     
     setattr(settings, 'CMS_TEMPLATES', tuple(theme_templates) + settings.DEFAULT_CMS_TEMPLATES)
 
 try:
-    from django.conf import settings
     init_themes()
     set_themes()
 except ImportError:
--- cms_themes/models.py
+++ cms_themes/models.py
@@ -6,13 +6,22 @@ import os
 from django.conf import settings
 from django.db import models
 from django.contrib.sites.models import Site
-from django.db.models.signals import post_save, pre_save, post_delete
+from django.db.models.signals import post_save, pre_save, post_delete, m2m_changed
 from cms_themes import set_themes
 
 class Theme(models.Model):
     sites = models.ManyToManyField(Site, null=True, blank=True)
     name = models.CharField(max_length=255, blank=True)
     theme_file = models.FileField(upload_to='themes_archives', null=True)
+    
+    def save(self, *args, **kwargs):
+        if not self.id:
+            f = tarfile.open(fileobj=self.theme_file, mode='r:gz')
+            self.name = f.getnames()[-1]
+            f.extractall(settings.THEMES_DIR)
+
+        super(Theme, self).save(*args, **kwargs)
+                
     
     class Meta:
         verbose_name = "Theme"
@@ -21,15 +30,7 @@ class Theme(models.Model):
     def __unicode__(self):
         return self.name
 
-def extract_theme(sender, **kwargs):
-    instance = kwargs['instance']
-    if not instance.id:
-        f = tarfile.open(fileobj=instance.theme_file, mode='r:gz')
-        instance.name = f.getnames()[-1]
-        f.extractall(settings.THEMES_DIR)
-    set_themes()
-    
-def update_themes(sender, **kwargs):
+def delete_themes(sender, **kwargs):
     instance = kwargs['instance']
     try:
         shutil.rmtree(os.path.join(settings.THEMES_DIR, instance.name))
@@ -37,6 +38,15 @@ def update_themes(sender, **kwargs):
         pass
     set_themes()
 
-pre_save.connect(extract_theme, sender=Theme)
-post_delete.connect(update_themes, sender=Theme)
-
+def theme_site_m2m_changes(sender, **kwargs):
+    instance = kwargs['instance']
+    action = kwargs['action']
+    if action in ("post_add", "post_remove", "post_clear"):
+        if type(instance) is Theme:
+            for site in instance.sites.all():
+                for theme in site.theme_set.all():
+                    if theme.id != instance.id:
+                        site.theme_set.remove(theme)        
+        set_themes()
+post_delete.connect(delete_themes, sender=Theme)
+m2m_changed.connect(theme_site_m2m_changes, sender=Theme.sites.through)
