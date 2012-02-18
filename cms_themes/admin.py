1f8e4dea768845b8d7faecaf01beeefd17b4b375
Iacopo Spalletti <i.spalletti@nephila.it>
2012-02-18 11:03:00 +0100

    Dirty hack to get PageAdmin to see the themes' templates

--- cms_themes/admin.py
+++ cms_themes/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin, messages
 from models import Theme
 from django.conf import settings
+from django.utils.translation import ugettext, ugettext_lazy as _
+from django.db import models
 
 class ThemeAdmin(admin.ModelAdmin):
     list_display = ('id','name',)
@@ -9,4 +11,14 @@ class ThemeAdmin(admin.ModelAdmin):
     filter_horizontal = ('sites',)
     readonly_fields = ('name',)
 admin.site.register(Theme, ThemeAdmin)
+
+from cms.models import Page
+from cms.admin.pageadmin import PageAdmin
+from cms.admin.forms import PageAddForm,PageForm
+admin.site.unregister(Page)
+t = Page._meta.get_field_by_name("template")[0]
+template_choices = [(x, _(y)) for x,y in settings.CMS_TEMPLATES]
+t.choices.extend(template_choices)
+admin.site.register(Page, PageAdmin)
+
 
